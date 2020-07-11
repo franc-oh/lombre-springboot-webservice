1f3eb98a5bf65db9e78bbf375dcbb7707b5b2e96
france1988 <france1988@naver.com>
2020-07-11 20:47:42 +0900

    20200711_view Finished(mustache)

--- src/main/java/com/lombre/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/lombre/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.lombre.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query(value = "SELECT p.* FROM POSTS p ORDER BY p.id DESC", nativeQuery = true)
+    List<Posts> findAllDesc();
 }
---
+++ src/main/java/com/lombre/book/springboot/web/IndexController.java
@@ -0,0 +1,43 @@
+package com.lombre.book.springboot.web;
+
+import com.lombre.book.springboot.posts.service.PostsService;
+import com.lombre.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    /**
+     * 화면 이동_메인 페이지
+     */
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    /**
+     * 화면 이동_글 등록 페이지
+     */
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    /**
+     * 화면 이동_글 수정 페이지
+     */
+    @GetMapping("/posts/update/{id}")
+    public String postsSave(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+}
